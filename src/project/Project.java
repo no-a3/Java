a6e5372353fe86589cdd6ff1dcad8934e36cc606
no-a3 <sin.h33990@gmail.com>
2021-02-28 10:45:44 +0300

    first commit

---
+++ src/project/Project.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+
+/**
+ *
+ * @author USER
+ */
+public class Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int s=0;
+        int x=7;
+        int y=8;
+        int z=112345;
+        s=z;
+        System.out.println(""+x+y+s);
+    }
+    
+}
