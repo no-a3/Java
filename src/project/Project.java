fb6a19367616518042ae2b42cca4ac127d0a5a4b
kira <ab@mkka,com>
2021-03-01 03:29:40 +0300

    just add commint and color output

--- src/project/Project.java
+++ src/project/Project.java
@@ -19,51 +19,58 @@ public class Project {
     /**
      * @param args the command line arguments
      */
+    
+    public static final String ANSI_GREEN = "\u001B[32m"; // green color 
     public static void main(String[] args) {
-        pro();
-    }
-        
-
-    public static void pro() {
+        for (;;) {
+            // here get the date time
         Date date = new Date(); // Date object 
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date); // set time 
         int year = calendar.get(Calendar.YEAR); // now year 
 
+        // input object
         Scanner commandObj = new Scanner(System.in); // scanner command input
         Scanner ageObj = new Scanner(System.in); // scanner age input 
-        String cmd;
-        System.out.print("Enter command :");
-        cmd = commandObj.nextLine();
-    
         
-            int age;
-            do {
-                System.out.print("Please enter your age : ");
-                while (!ageObj.hasNextInt()) {
-                    System.out.println("That's not a number!");
-                    System.out.print("Please enter your age : ");
-                    ageObj.next();
+        // input value
+        String cmd;
+        int age;
 
+        System.out.print(ANSI_GREEN + "Enter command :");
+        cmd = commandObj.nextLine();
+            // validation age for get just number 
+            do {
+                System.out.print(ANSI_GREEN + "Please enter your age : ");
+                while (!ageObj.hasNextInt()) {
+                    System.out.println(ANSI_GREEN + "That's not a number!");
+                    System.out.print(ANSI_GREEN + "Please enter your age : ");
+                    ageObj.next();
                 }
                 age = ageObj.nextInt();
             } while (age <= 0);
-
+            // end validation
+        
             switch (cmd) {
                 case "-y":
+                    System.out.print(ANSI_GREEN + "your age in years is ");
                     System.out.println(year - age);
                     break;
                 case "-m":
+                System.out.print(ANSI_GREEN + "your age in months is ");
                     System.out.println(age * 12);
                     break;
                 case "-d":
+                System.out.print(ANSI_GREEN + "your age in days is ");
                     System.out.println(age * 365);
                     break;
                 default:
-                    System.out.println("this is not command");
+                    System.out.println(ANSI_GREEN + "this is not command ");
                     break;
             }
+        }
     }
+        
 
     }
 
