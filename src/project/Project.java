cb8a69ae129e9423589f45313e419ae18696a41b
kira <ab@mkka,com>
2021-03-02 13:52:00 +0300

    nested loop and if

--- src/project/Project.java
+++ src/project/Project.java
@@ -22,9 +22,11 @@ public class Project {
     public static final String ANSI_RED = "\u001B[31m"; // red color 
 
     public static void main(String[] args) {
-        Headers(); //headers
         // infiniteLoop
-        for (;;) {
+        for(;;) {
+            for (int i = 0; i == 0; i++) {
+                 Headers(); //headers
+            }
             // here get the date time
             Date date = new Date(); // Date object 
             Calendar calendar = Calendar.getInstance();
@@ -38,9 +40,9 @@ public class Project {
             // input value
             String cmd;
             int age;
-
             System.out.print(ANSI_RED + "Enter command :");
             cmd = commandObj.nextLine();
+        
             // validation age for get just number 
             do {
                 System.out.print(ANSI_RED + "Please enter your age : ");
@@ -50,26 +52,32 @@ public class Project {
                     ageObj.next();
                 }
                 age = ageObj.nextInt();
-            } while (age <= 0);
+            } while (age <= -1);
             // end validation
-
-            switch (cmd) {
-                case "-y":
-                    System.out.print(ANSI_RED + "your age in years is ");
-                    System.out.println(year - age);
-                    break;
-                case "-m":
-                    System.out.print(ANSI_RED + "your age in months is ");
-                    System.out.println(age * 12);
-                    break;
-                case "-d":
-                    System.out.print(ANSI_RED + "your age in days is ");
-                    System.out.println(age * 365);
-                    break;
-                default:
-                    System.out.println(ANSI_RED + "this is not command ");
-                    break;
-            }
+            if (age != 0) {
+                if (age > 7) {
+                    switch (cmd) {
+                        case "-y":
+                            System.out.print(ANSI_RED + "your age in years is ");
+                            System.out.println(year - age);
+                            break;
+                        case "-m":
+                            System.out.print(ANSI_RED + "your age in months is ");
+                            System.out.println(age * 12);
+                            break;
+                        case "-d":
+                            System.out.print(ANSI_RED + "your age in days is ");
+                            System.out.println(age * 365);
+                            break;
+                        default:
+                            System.out.println(ANSI_RED + "this is not command ");
+                            break;
+                    } 
+                } else {
+                    System.out.println("your age most be > 0");}
+            } else {
+                System.out.println("your age can't be 0");}
+        
         } // end infiniteLoop
     }
 
